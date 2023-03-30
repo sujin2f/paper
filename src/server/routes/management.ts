20d281ed8f8fde3689796fd472a1362546c0286e
Sujin Choi <sujin.2f@gmail.com>
2023-03-30 13:07:04 -0400

    Update management.ts

--- src/server/routes/management.ts
+++ src/server/routes/management.ts
@@ -14,8 +14,28 @@ const managementRouter = express.Router()
  */
 managementRouter.get('/update', (req, res) => {
     if (process.env.MANAGEMENT) {
-        exec('git pull', () => {
-            exec('yarn prod')
+        exec('git pull', (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+
+            exec('yarn prod', (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            })
         })
     }
 })
