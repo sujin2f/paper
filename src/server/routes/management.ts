8bbbd3164823cbe568744c0660516d024a2cf727
Sujin Choi <sujin.2f@gmail.com>
2023-03-30 13:02:45 -0400

    management

---
+++ src/server/routes/management.ts
@@ -0,0 +1,23 @@
+/* istanbul ignore file */
+import express, { Response, Request } from 'express'
+import path from 'path'
+import ejs from 'ejs'
+import { exec } from 'child_process'
+
+import { GlobalVariable } from 'src/types/common'
+import { bundles, publicDir, baseDir } from 'src/utils/environment'
+
+const managementRouter = express.Router()
+
+/**
+ * Static Dir
+ */
+staticRouter.get('/update', (req, res) => {
+    if (process.env.MANAGEMENT) {
+        exec('git pull', () => {
+            exec('yarn prod')
+        })
+    }
+})
+
+export { staticRouter }
