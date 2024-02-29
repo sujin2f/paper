629c69e65207fd646ba28d0be85eaa08c1dabe09
Sujin Choi <schoi@postmedia.com>
2024-02-29 00:26:06 -0500

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,43 +1,67 @@
-const CACHE_NAME = 'ether-cache'
+const PRECACHE = 'ether-cache-v1'
+const RUNTIME = 'ether-cache-runtime-v1'
+const PRE_CACHE_URLS = ['index.html', './']
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache
-                .addAll(['/', '/index.html'])
-                .then(() => self.skipWaiting())
-        }),
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(PRE_CACHE_URLS))
+            .then(() => self.skipWaiting()),
     )
 })
 
-self.addEventListener('fetch', (e) => {
-    if (navigator.onLine) {
-        const fetchRequest = e.request.clone()
-        return fetch(fetchRequest).then((response) => {
-            if (
-                response.url.indexOf('http') !== 0 ||
-                fetchRequest.method !== 'GET' ||
-                !response ||
-                response.status !== 200 ||
-                response.type !== 'basic'
-            ) {
-                return response
-            }
-
-            const responseCache = response.clone()
-
-            caches.open(CACHE_NAME).then((cache) => {
-                cache.put(e.request, responseCache)
+self.addEventListener('activate', (e) => {
+    const currentCaches = [PRECACHE, RUNTIME]
+    e.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter(
+                    (cacheName) => !currentCaches.includes(cacheName),
+                )
             })
-            return response
-        })
-    } else {
-        e.responseWith(
-            caches.match(e.request).then((response) => {
-                if (response) {
-                    return response
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete)
+                    }),
+                )
+            })
+            .then(() => self.clients.claim()),
+    )
+})
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            (async () => {
+                const cachedResponse = await caches.match(event.request)
+                if (cachedResponse) {
+                    return cachedResponse
                 }
-            }),
+
+                const request = event.request.clone()
+
+                if (
+                    request.method !== 'GET' ||
+                    request.url.indexOf('http') !== 0
+                ) {
+                    return await fetch(request)
+                }
+
+                return await caches.open(RUNTIME).then(async (cache) => {
+                    return await fetch(request).then(async (response) => {
+                        if (!response || response.status !== 200) {
+                            return response
+                        }
+
+                        return await cache
+                            .put(event.request, response.clone())
+                            .then(() => response)
+                    })
+                })
+            })(),
         )
     }
 })
